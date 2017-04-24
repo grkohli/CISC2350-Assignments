ed2c2023379dfb651f422c3895326d569562eac1
Yuraima <yurm04@gmail.com>
2017-04-23 20:21:47 -0400

    added review for assignments 6

--- assignment8/index.php
+++ assignment8/index.php
@@ -8,25 +8,33 @@
 </head>
 
 <body>
-	<?php 
-		define('MY_NAME', "Rosie Kohli");	//defined my name as a constant variable (strings). 
+	<?php
+		define('MY_NAME', "Rosie Kohli");	//defined my name as a constant variable (strings).
 		$age = 19;
-		$city = "New York City";			
+		$city = "New York City";
 		$birthday = "June 23, 1997";	//variable that states my birthday. It's a string even if it contains numbers because it's a string of chars.
 		$favoritefoods = array("Thai Food", "Twice Baked Potato", "Mint Chocolate Chip Ice Cream" ); //indexed array containing my three favorite foods
 		$classes = array("CS2350" => "Information and Web Programming", "CS4080" => "Computer Algorithms", "MATH2005" => "Multivariate Calculus", "CS4090" => "Theory of Computation" );    //associative array containing four classes
 		$earned_credits = 51;
 		$total_credits = 124;
-		$credits_needed;	
+		$credits_needed;
 		$students = array(
+			/*
+				YE
+				you modified the data array -2pts
+			 */
 			'Rick' => 124,
 			'Morty' => 24,
 			'Summer' => 48,
 			'Beth' => 75,
 			'Jerry' => 32,
 			'Bird_man' => 97
-		);	
-	 
+		);
+
+	 /*
+	 	YE
+	 	great function! please make sure you are adding comments
+	  */
 	 function get_student_year( $earned_credits) {
 	 		$freshmancredits = 30;
 			$sophomorecredits = 60;
@@ -34,45 +42,50 @@
 			$seniorcredits = 124;
 			$classification;
 
-			echo "<p>Credits I Currently Have == $earned_credits</p>"; 
-			
+			echo "<p>Credits I Currently Have == $earned_credits</p>";
+
 		 		if($earned_credits >= 0 && $earned_credits <= $freshmancredits){
 		 			$classification = "Freshman";
 					echo "Classified as a freshman. ";
-					
+
 					return( $classification);
 		 		}
 		 		elseif ($earned_credits > $freshmancredits && $earned_credits <= $sophomorecredits) {
 		 			$classification = "Sophomore";
 					echo "Classified as a sophomore. ";
-					
+
 					return( $classification);
 		 		}
 		 		elseif ($earned_credits > $sophomorecredits && $earned_credits <= $juniorcredits) {
 		 			$classification = "Junior";
 					echo "Classified as a junior. ";
-					
+
 					return( $classification);
 		 		}
 		 		elseif ($earned_credits > $juniorcredits && $earned_credits <= $seniorcredits) {
 		 			$classification = "Senior";
 					echo "Classified as a senior. ";
-					
+
 					return( $classification);
 		 		}
-		  	
+
 	}
 
 	function get_grad_credits( $earned_credits){
 		$total_credits = 124;
-
-		echo "<p>Earned Credits == $earned_credits</p>"; 
+		/*
+			YE
+			if you are returning the value, you don't need to echo
+			it in the function.  Just echo the return value when it is
+			returned
+		 */
+		echo "<p>Earned Credits == $earned_credits</p>";
 		echo "<p>Credits Needed to Graduate== $total_credits</p>";
-	
+
 		$credits_needed = $total_credits - $earned_credits;
-		
-		echo "<p>Remaining Credits Needed to Graduate== $credits_needed</p>"; 
-		
+
+		echo "<p>Remaining Credits Needed to Graduate== $credits_needed</p>";
+
 		return( $credits_needed);
 	}
 
@@ -83,12 +96,17 @@
 			$seniorcredits = 124;
 			$nextclassification;
 
-			echo "<p>Credits I Currently Have == $earned_credits</p>"; 
-			
+			echo "<p>Credits I Currently Have == $earned_credits</p>";
+
 		 		if($earned_credits >= 0 && $earned_credits <= $freshmancredits){
 					$credits_needed = $freshmancredits - $earned_credits;
 					$nextclassification = "Sophomore";
 		 			echo " Needs " . $credits_needed . " credits to become a sophomore." ;
+		 			/*
+		 				YE
+		 				there can only be 1 return statement.  You need to use a specific datastructure
+						to return both the credits and the classification -5pts
+		 			 */
 		 			return( $credits_needed);
 		 			return ($nextclassification);
 		 		}
@@ -119,8 +137,8 @@
 	?>
 
 	<section>
-		<h1><?php echo MY_NAME; ?></h1>		
-			<p><?php echo "Birthday: " . $birthday; ?></p>	
+		<h1><?php echo MY_NAME; ?></h1>
+			<p><?php echo "Birthday: " . $birthday; ?></p>
 			<p><?php echo "Age: " . $age; ?></p>
 			<p><?php echo $city;  ?></p>
 
@@ -145,21 +163,29 @@
 	</section>
 
 	<section>
-		<?php  
+		<?php
 		$count = 1;
 		foreach( $students as $name => $earned_credits) {
 			echo "<p>------STUDENT $count:------</p>";
 			echo "<p>The student name is $name and he/she earned $earned_credits credits</p>";
-			
-			get_grad_credits($earned_credits); 
-	
-			get_student_year($earned_credits); 
+
+			get_grad_credits($earned_credits);
+
+			get_student_year($earned_credits);
 
 			get_next_level($earned_credits);
 
+			/*
+				YE
+				you are in a forech loop, no need for a counter -2pts
+			 */
 			$count++;
 		}
-		
+
+		/*
+			YE
+			did not use built in functions to format names -10pts
+		 */
 		?>
 	</section>
 
